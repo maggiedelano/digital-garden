526d061688b4b3657a5fb07a184eac191e1263b6
Maggie Delano <maggie.delano@gmail.com>
2021-03-28 17:39:46 -0400

    comment out how bidirectional links generator handles dead page links; was breaking things

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -55,15 +55,15 @@ class BidirectionalLinksGenerator < Jekyll::Generator
       # At this point, all remaining double-bracket-wrapped words are
       # pointing to non-existing pages, so let's turn them into disabled
       # links by greying them out and changing the cursor
-      current_note.content = current_note.content.gsub(
-        /\[\[(.*)\]\]/i, # match on the remaining double-bracket links
-        <<~HTML.chomp    # replace with this HTML (\\1 is what was inside the brackets)
-          <span title='There is no note that matches this link.' class='invalid-link'>
-            <span class='invalid-link-brackets'>[[</span>
-            \\1
-            <span class='invalid-link-brackets'>]]</span></span>
-        HTML
-      )
+      #current_note.content = current_note.content.gsub(
+      #  /\[\[(.*)\]\]/i, # match on the remaining double-bracket links
+      #  <<~HTML.chomp    # replace with this HTML (\\1 is what was inside the brackets)
+      #    <span title='There is no note that matches this link.' class='invalid-link'>
+      #      <span class='invalid-link-brackets'>[[</span>
+      #      \\1
+      #      <span class='invalid-link-brackets'>]]</span></span>
+      #  HTML
+      #)
     end
 
     # Identify note backlinks and add them to each note
